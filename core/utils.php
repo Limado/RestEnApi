8d7639bb072de1a111297c51d92be1c282b91470
eminoli <noliemiliano@gmail.com>
2020-07-04 17:35:49 -0300

    Arreglo en getAllHeaders por si no esta en un apache o no reconoce la funcion.

--- core/utils.php
+++ core/utils.php
@@ -122,3 +122,23 @@ class ApiLogger
         chmod($this->getPath() . $fileName, $this->mode);
     }
 }
+
+/**
+ * En caso que el servidor no sea apache o no reconozca la funcion
+ */
+
+if (!function_exists('getallheaders'))
+{
+    function getallheaders()
+    {
+           $headers = [];
+       foreach ($_SERVER as $name => $value)
+       {
+           if (substr($name, 0, 5) == 'HTTP_')
+           {
+               $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+           }
+       }
+       return $headers;
+    }
+}
